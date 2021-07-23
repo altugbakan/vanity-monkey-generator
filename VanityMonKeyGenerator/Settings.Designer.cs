56402711c8057124c1e1763d61aed79f705a8e87
Begüm Bakan <begumbakan2005@gmail.com>
2021-07-23 11:49:39 +0300

    Added Settings form.

---
+++ VanityMonKeyGenerator/Settings.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace VanityMonKeyGenerator
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glassesComboBox = new System.Windows.Forms.ComboBox();
+            this.glassesLabel = new System.Windows.Forms.Label();
+            this.hatsComboBox = new System.Windows.Forms.ComboBox();
+            this.hatsLabel = new System.Windows.Forms.Label();
+            this.miscComboBox = new System.Windows.Forms.ComboBox();
+            this.miscLabel = new System.Windows.Forms.Label();
+            this.mouthsComboBox = new System.Windows.Forms.ComboBox();
+            this.mouthsLabel = new System.Windows.Forms.Label();
+            this.shirtPantsComboBox = new System.Windows.Forms.ComboBox();
+            this.shirtPantsLabel = new System.Windows.Forms.Label();
+            this.shoesComboBox = new System.Windows.Forms.ComboBox();
+            this.shoesLabel = new System.Windows.Forms.Label();
+            this.tailsComboBox = new System.Windows.Forms.ComboBox();
+            this.tailsLabel = new System.Windows.Forms.Label();
+            this.monKeyPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.monKeyPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glassesComboBox
+            // 
+            this.glassesComboBox.FormattingEnabled = true;
+            this.glassesComboBox.Items.AddRange(new object[] {
+            "Any",
+            "None",
+            "Eye Patch",
+            "Glasses Nerd Cyan",
+            "Glasses Nerd Green",
+            "Glasses Nerd Pink",
+            "Monocle",
+            "Sunglasses Aviator Cyan",
+            "Sunglasses Aviator Green",
+            "Sunglasses Aviator Yellow",
+            "Sunglasses Thug"});
+            this.glassesComboBox.Location = new System.Drawing.Point(28, 332);
+            this.glassesComboBox.Name = "glassesComboBox";
+            this.glassesComboBox.Size = new System.Drawing.Size(186, 33);
+            this.glassesComboBox.TabIndex = 0;
+            this.glassesComboBox.Text = "Any";
+            this.glassesComboBox.SelectedIndexChanged += new System.EventHandler(this.glassesComboBox_SelectedIndexChanged);
+            // 
+            // glassesLabel
+            // 
+            this.glassesLabel.AutoSize = true;
+            this.glassesLabel.Location = new System.Drawing.Point(86, 304);
+            this.glassesLabel.Name = "glassesLabel";
+            this.glassesLabel.Size = new System.Drawing.Size(70, 25);
+            this.glassesLabel.TabIndex = 1;
+            this.glassesLabel.Text = "Glasses";
+            // 
+            // hatsComboBox
+            // 
+            this.hatsComboBox.FormattingEnabled = true;
+            this.hatsComboBox.Items.AddRange(new object[] {
+            "Any",
+            "None",
+            "Bandana",
+            "Beanie",
+            "Beanie Banano",
+            "Beanie Hippie",
+            "Beanie Long",
+            "Beanie Long Banano",
+            "Cap",
+            "Cap Backwards",
+            "Cap Banano",
+            "Cap Bebe",
+            "Cap Carlos",
+            "Cap Hng",
+            "Cap Hng Plus",
+            "Cap Kappa",
+            "Cap Pepe",
+            "Cap Rick",
+            "Cap Smug",
+            "Cap Smug Green",
+            "Cap Thonk",
+            "Crown",
+            "Fedora",
+            "Fedora Long",
+            "Hat Cowboy",
+            "Hat Jester",
+            "Helmet Viking"});
+            this.hatsComboBox.Location = new System.Drawing.Point(313, 332);
+            this.hatsComboBox.Name = "hatsComboBox";
+            this.hatsComboBox.Size = new System.Drawing.Size(182, 33);
+            this.hatsComboBox.TabIndex = 2;
+            this.hatsComboBox.Text = "Any";
+            // 
+            // hatsLabel
+            // 
+            this.hatsLabel.AutoSize = true;
+            this.hatsLabel.Location = new System.Drawing.Point(380, 304);
+            this.hatsLabel.Name = "hatsLabel";
+            this.hatsLabel.Size = new System.Drawing.Size(48, 25);
+            this.hatsLabel.TabIndex = 3;
+            this.hatsLabel.Text = "Hats";
+            // 
+            // miscComboBox
+            // 
+            this.miscComboBox.FormattingEnabled = true;
+            this.miscComboBox.Items.AddRange(new object[] {
+            "Banana Hands",
+            "Banana Right Hand",
+            "Bowtie",
+            "Camera",
+            "Club",
+            "Flamethrower",
+            "Gloves White",
+            "Guitar",
+            "Microphone",
+            "Necklace Boss",
+            "Tie Cyan",
+            "Tie Pink",
+            "Whiskey Right"});
+            this.miscComboBox.Location = new System.Drawing.Point(585, 332);
+            this.miscComboBox.Name = "miscComboBox";
+            this.miscComboBox.Size = new System.Drawing.Size(182, 33);
+            this.miscComboBox.TabIndex = 4;
+            this.miscComboBox.Text = "Any";
+            // 
+            // miscLabel
+            // 
+            this.miscLabel.AutoSize = true;
+            this.miscLabel.Location = new System.Drawing.Point(652, 304);
+            this.miscLabel.Name = "miscLabel";
+            this.miscLabel.Size = new System.Drawing.Size(48, 25);
+            this.miscLabel.TabIndex = 5;
+            this.miscLabel.Text = "Misc";
+            // 
+            // mouthsComboBox
+            // 
+            this.mouthsComboBox.FormattingEnabled = true;
+            this.mouthsComboBox.Items.AddRange(new object[] {
+            "Any",
+            "None",
+            "Cigar",
+            "Confused",
+            "Joint",
+            "Meh",
+            "Pipe",
+            "Smile Big Teeth",
+            "Smile Normal",
+            "Smile Tongue"});
+            this.mouthsComboBox.Location = new System.Drawing.Point(30, 403);
+            this.mouthsComboBox.Name = "mouthsComboBox";
+            this.mouthsComboBox.Size = new System.Drawing.Size(182, 33);
+            this.mouthsComboBox.TabIndex = 6;
+            this.mouthsComboBox.Text = "Any";
+            // 
+            // mouthsLabel
+            // 
+            this.mouthsLabel.AutoSize = true;
+            this.mouthsLabel.Location = new System.Drawing.Point(85, 375);
+            this.mouthsLabel.Name = "mouthsLabel";
+            this.mouthsLabel.Size = new System.Drawing.Size(73, 25);
+            this.mouthsLabel.TabIndex = 7;
+            this.mouthsLabel.Text = "Mouths";
+            this.mouthsLabel.Click += new System.EventHandler(this.mouthsLabel_Click);
+            // 
+            // shirtPantsComboBox
+            // 
+            this.shirtPantsComboBox.FormattingEnabled = true;
+            this.shirtPantsComboBox.Items.AddRange(new object[] {
+            "Any",
+            "None",
+            "Overalls Blue",
+            "Overalls Red",
+            "Pants Business Blue",
+            "Pants Flower",
+            "Tshirt Long Stripes",
+            "Tshirt Short White"});
+            this.shirtPantsComboBox.Location = new System.Drawing.Point(313, 403);
+            this.shirtPantsComboBox.Name = "shirtPantsComboBox";
+            this.shirtPantsComboBox.Size = new System.Drawing.Size(182, 33);
+            this.shirtPantsComboBox.TabIndex = 8;
+            this.shirtPantsComboBox.Text = "Any";
+            // 
+            // shirtPantsLabel
+            // 
+            this.shirtPantsLabel.AutoSize = true;
+            this.shirtPantsLabel.Location = new System.Drawing.Point(356, 375);
+            this.shirtPantsLabel.Name = "shirtPantsLabel";
+            this.shirtPantsLabel.Size = new System.Drawing.Size(97, 25);
+            this.shirtPantsLabel.TabIndex = 9;
+            this.shirtPantsLabel.Text = "Shirt-Pants";
+            // 
+            // shoesComboBox
+            // 
+            this.shoesComboBox.FormattingEnabled = true;
+            this.shoesComboBox.Items.AddRange(new object[] {
+            "Any",
+            "None",
+            "Sneakers Blue",
+            "Sneakers Green",
+            "Sneakers Red",
+            "Sneakers Swagger",
+            "Socks H Stripe",
+            "Socks V Stripe"});
+            this.shoesComboBox.Location = new System.Drawing.Point(585, 403);
+            this.shoesComboBox.Name = "shoesComboBox";
+            this.shoesComboBox.Size = new System.Drawing.Size(182, 33);
+            this.shoesComboBox.TabIndex = 10;
+            this.shoesComboBox.Text = "Any";
+            // 
+            // shoesLabel
+            // 
+            this.shoesLabel.AutoSize = true;
+            this.shoesLabel.Location = new System.Drawing.Point(646, 375);
+            this.shoesLabel.Name = "shoesLabel";
+            this.shoesLabel.Size = new System.Drawing.Size(60, 25);
+            this.shoesLabel.TabIndex = 11;
+            this.shoesLabel.Text = "Shoes";
+            // 
+            // tailsComboBox
+            // 
+            this.tailsComboBox.FormattingEnabled = true;
+            this.tailsComboBox.Items.AddRange(new object[] {
+            "Any",
+            "None",
+            "Tail Sock"});
+            this.tailsComboBox.Location = new System.Drawing.Point(313, 469);
+            this.tailsComboBox.Name = "tailsComboBox";
+            this.tailsComboBox.Size = new System.Drawing.Size(182, 33);
+            this.tailsComboBox.TabIndex = 12;
+            this.tailsComboBox.Text = "Any";
+            // 
+            // tailsLabel
+            // 
+            this.tailsLabel.AutoSize = true;
+            this.tailsLabel.Location = new System.Drawing.Point(382, 441);
+            this.tailsLabel.Name = "tailsLabel";
+            this.tailsLabel.Size = new System.Drawing.Size(44, 25);
+            this.tailsLabel.TabIndex = 13;
+            this.tailsLabel.Text = "Tails";
+            // 
+            // monKeyPictureBox
+            // 
+            this.monKeyPictureBox.Location = new System.Drawing.Point(271, 25);
+            this.monKeyPictureBox.Name = "monKeyPictureBox";
+            this.monKeyPictureBox.Size = new System.Drawing.Size(276, 276);
+            this.monKeyPictureBox.TabIndex = 14;
+            this.monKeyPictureBox.TabStop = false;
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.monKeyPictureBox);
+            this.Controls.Add(this.tailsLabel);
+            this.Controls.Add(this.tailsComboBox);
+            this.Controls.Add(this.shoesLabel);
+            this.Controls.Add(this.shoesComboBox);
+            this.Controls.Add(this.shirtPantsLabel);
+            this.Controls.Add(this.shirtPantsComboBox);
+            this.Controls.Add(this.mouthsLabel);
+            this.Controls.Add(this.mouthsComboBox);
+            this.Controls.Add(this.miscLabel);
+            this.Controls.Add(this.miscComboBox);
+            this.Controls.Add(this.hatsLabel);
+            this.Controls.Add(this.hatsComboBox);
+            this.Controls.Add(this.glassesLabel);
+            this.Controls.Add(this.glassesComboBox);
+            this.Name = "Settings";
+            this.Text = "Settings";
+            ((System.ComponentModel.ISupportInitialize)(this.monKeyPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox glassesComboBox;
+        private System.Windows.Forms.Label glassesLabel;
+        private System.Windows.Forms.ComboBox hatsComboBox;
+        private System.Windows.Forms.Label hatsLabel;
+        private System.Windows.Forms.ComboBox miscComboBox;
+        private System.Windows.Forms.Label miscLabel;
+        private System.Windows.Forms.ComboBox mouthsComboBox;
+        private System.Windows.Forms.Label mouthsLabel;
+        private System.Windows.Forms.ComboBox shirtPantsComboBox;
+        private System.Windows.Forms.Label shirtPantsLabel;
+        private System.Windows.Forms.ComboBox shoesComboBox;
+        private System.Windows.Forms.Label shoesLabel;
+        private System.Windows.Forms.ComboBox tailsComboBox;
+        private System.Windows.Forms.Label tailsLabel;
+        private System.Windows.Forms.PictureBox monKeyPictureBox;
+    }
+}
---
+++ VanityMonKeyGenerator/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VanityMonKeyGenerator
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void glassesComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mouthsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
